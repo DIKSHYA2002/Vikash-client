6c79b5dfde30492bf2d79480e13388befd3fda1d
dikshya2002 <ddikshya.agarwal2002@gmail.com>
2022-09-06 10:26:05 +0530

    successful image upload-products

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,7 +14,6 @@ export function Products(){
         const data = await fetch("http://localhost:4001/products");
         const items_data = await data.json();
         ProductCtx.setItems(items_data.products);
-        console.log( ProductCtx.items)
       }
 
     return (
@@ -23,10 +22,10 @@ export function Products(){
         <h1>Products</h1>
         <div className='ProductsList'>
                         <div className='product' >
-                                        {ProductCtx.items.map((singleData,index) => {
-                                    const base64String = btoa(  String.fromCharCode(...new Uint8Array(singleData.ProductImage.data.data))  );
-                                    return  <div className='item-container'key={index}>
-                                   <img src={`data:image/png;base64,${base64String}`}  width="300"></img>
+                                    {ProductCtx.items.map((singleData,index) => {
+                                   const base64String = btoa( String.fromCharCode(...new Uint8Array(singleData.ProductImage.data.data))  );
+                                    return <div className='item-container'key={index}>
+                                    <img src={`data:image/png;base64,${base64String}`}  width="300"></img>
                                     <div className='Name'><h3>{singleData.Name}</h3></div>
                                     <div className='Price'><h3>{singleData.Price}</h3></div>
                                     <div className='Description'>
@@ -37,6 +36,7 @@ export function Products(){
                                  
                                 </div>
                                     
+                                    
                                 })}
                         
                         </div>
@@ -44,4 +44,4 @@ export function Products(){
     </div>
     )
 }
-  
+//  
