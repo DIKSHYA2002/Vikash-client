ad4b3cb088f30c11d871e8932ab9c238a4f03b11
Sibasish Satapathy <cse.20bcsa33@silicon.ac.in>
2022-09-11 10:54:52 +0530

    Added the loading component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,20 @@
 import logo from '../images/logo.png'
-// import './Navbar.css';
-
-// export function Navbar(){
-//     return (
-//          <div className="nav2">
-//    <div className="links">
-//     <ul>
-//        <li><Link to = "/">HOME</Link></li>
-//         <li><Link to = "/create">CREATE</Link></li>
-//         <li><Link to = "/Blogs">BLOGS</Link></li>
-//         <li><Link to = "/products">PRODUCTS</Link></li>
-//         <li><Link to = "/services">SERVICES</Link></li>
-//         <li><Link to = "/about">ABOUT</Link></li>
-//     </ul>
-//     <div className="logo">
-//        <img src={logo} ></img>
-//     </div>
-// </div>
-
-// </div> 
-//     )
-
-// }
-
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import Loading from './Loading';
 
-export const Navbar = ()=> {
+export const Navbar = () => {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
 
-    const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+    const { loginWithRedirect, isAuthenticated, user, isLoading } = useAuth0();
     const { logout } = useAuth0();
-
+    if (isLoading) {
+        return (<Loading />);
+    }
     return (
         <>
             <nav className="navbar">
@@ -98,24 +77,13 @@ export const Navbar = ()=> {
                                     Services
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink
-                                    exact
-                                    to="/about"
-                                    activeClassName="active"
-                                    className="nav-links"
-                                    onClick={handleClick}
-                                >
-                                    About
-                                </NavLink>
-                            </li>
                             <li>
                                 <button className="btn" onClick={() => logout({ returnTo: window.location.origin })}>
                                     Log Out
                                 </button>
                             </li>
                         </ul>) : (<li><button className="btn" onClick={() => loginWithRedirect()}>Log In</button></li>)}
-                        <li className="nav-item">{isAuthenticated && <p className='user'>{user.email}</p>}</li>
+                        <li className="nav-item">{isAuthenticated && <img className='user' src={user.picture} alt="" />}</li>
                     </ul>
                     <div className="nav-icon" onClick={handleClick}>
                         <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
@@ -125,5 +93,3 @@ export const Navbar = ()=> {
         </>
     );
 }
-
-// export default Navbar;
--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -2,16 +2,16 @@ import React from "react";
 import './landing-page.css';
 import { Navbar } from "./Navbar";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import { Link } from "react-router-dom";
+import Loading from "./Loading";
 
 export function Homepage() {
-    const { loginWithRedirect} = useAuth0();
-   
+    const { loginWithRedirect, isAuthenticated, isLoading} = useAuth0();
+    if(isLoading){
+      return(<Loading />);
+    }
     return (
-        <>
-        
             <div className="home" >
-               
                 <Navbar />
                 <div className="head-1">
                     <div className="text--1">
@@ -27,12 +27,11 @@ export function Homepage() {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo <br></br>impedit quisquamnumquam earum minus quibusdam laudantium dolorem  <br></br>laboriosam quas ut? Quis adipisci necessitatibus quisquam ipsa.
                     </div>
                     <div className="btn-getStarted">
-                        <h1><button onClick={() => loginWithRedirect()}>Get Started</button></h1>
+                        <h1>{ isAuthenticated ?(<Link exact to="/Blogs" className="auth-button">Get Started</Link>):(<button onClick={() => loginWithRedirect()}>Get Started</button>)}</h1>
 
                     </div>
                 </div>
             </div>
-        </>
 
     );
 
